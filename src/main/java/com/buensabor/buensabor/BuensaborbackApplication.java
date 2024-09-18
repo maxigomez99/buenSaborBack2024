2df1e4f0ef6c7e7f48408983a833b7c3e256238d
maxigomez99 <gomez.maximiliano99@gmail.com>
2024-09-18 17:26:53 -0300

    Normalize line endings

---
+++ src/main/java/com/buensabor/buensabor/BuensaborbackApplication.java
@@ -0,0 +1,13 @@
+package com.buensabor.buensabor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuensaborbackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuensaborbackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/buensabor/buensabor/BuensaborbackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.buensabor.buensabor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuensaborbackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
