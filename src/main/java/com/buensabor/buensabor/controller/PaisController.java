b2215363671fcd1afc170845b3cdc20ece5c7003
maxigomez99 <gomez.maximiliano99@gmail.com>
2025-06-20 15:38:41 -0300

    controladores faltantes

---
+++ src/main/java/com/buensabor/buensabor/controller/PaisController.java
@@ -0,0 +1,68 @@
+package com.buensabor.buensabor.controller;
+
+import com.buensabor.buensabor.service.IPaisService;
+import com.buensabor.buensabor.entities.Pais;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/pais")
+public class PaisController {
+    @Autowired
+    private IPaisService paisService;
+
+    //region CRUD Basico
+
+    @GetMapping("/traer-todo/")
+    public ResponseEntity<?> mostrarLista(){
+        try {
+            return ResponseEntity.ok(paisService.buscarTodos());
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> buscarPorId(@PathVariable Long id){
+        try {
+            return ResponseEntity.ok(paisService.buscarPorId(id));
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+    @PostMapping("/")
+    public ResponseEntity<?> cargar(@RequestBody Pais pais){
+        try {
+            return ResponseEntity.ok(paisService.guardar(pais));
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<?> actualizar(@PathVariable Long id,@RequestBody Pais pais){
+        try {
+            return ResponseEntity.ok(paisService.modificar(id, pais));
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deleteById(@PathVariable Long id){
+        try {
+            return ResponseEntity.ok(paisService.eliminar(id));
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+    @PostMapping("/reactivate/{id}")
+    public ResponseEntity<?> reactivar(@PathVariable Long id){
+        try {
+            return ResponseEntity.ok(paisService.reactivar(id));
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+}
