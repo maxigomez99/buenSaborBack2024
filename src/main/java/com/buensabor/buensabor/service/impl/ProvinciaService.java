48ce7cc93afeea6dc61a010d2d414f57cc8bdc87
maxigomez99 <gomez.maximiliano99@gmail.com>
2025-06-21 12:09:11 -0300

    arreglo provincia

--- src/main/java/com/buensabor/buensabor/service/impl/ProvinciaService.java
+++ src/main/java/com/buensabor/buensabor/service/impl/ProvinciaService.java
@@ -19,65 +19,10 @@ public class ProvinciaService implements IProvinciaService {
     private IProvinciaRepository provinciaRepository;
     @Autowired
     private ProvinciaClient provinciaClient;
-    @Override
-    public Boolean eliminar(Long id) throws Exception {
-        try {
-            Provincia provincia = provinciaRepository.findById(id).get();
-            //provincia.setEliminado(true);
-            return true;
-        } catch (Exception e) {
-            throw new Exception(e.getMessage());
-        }
-    }
 
     @Override
-    public Provincia guardar(Provincia entity) throws Exception {
-        try {
-            return provinciaRepository.save(entity);
-        } catch (Exception e) {
-            throw new Exception(e.getMessage());
-        }
-    }
-
-    @Override
-    public Provincia modificar(Long id, Provincia entity) throws Exception {
-        try {
-            Provincia provincia = provinciaRepository.findById(id).get();
-            provincia.setNombre(entity.getNombre());
-            return provinciaRepository.save(provincia);
-        } catch (Exception e) {
-            throw new Exception(e.getMessage());
-        }
-    }
-
-    @Override
-    public Provincia buscarPorId(Long id) throws Exception {
-        try {
-            return provinciaRepository.findById(id).get();
-        } catch (Exception e) {
-            throw new Exception(e.getMessage());
-
-        }
-    }
-
-    @Override
-    public List<Provincia> buscarTodos() throws Exception {
-        try {
-            return provinciaRepository.findAll();
-        } catch (Exception e) {
-            throw new Exception(e.getMessage());
-        }
-    }
-
-    @Override
-    public Boolean reactivar(Long id) throws Exception {
-        try {
-            Provincia provincia = provinciaRepository.findById(id).get();
-            //provincia.setEliminado(false);
-            return true;
-        } catch (Exception e) {
-            throw new Exception(e.getMessage());
-        }
+    public List<Provincia> getProvinciaByPaisId(Long paisId) {
+        return provinciaRepository.findByPaisId(paisId);
     }
 
     public void saveProvinciasFromApi() throws IOException {
