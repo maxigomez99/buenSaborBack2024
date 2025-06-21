ce1c42ce855b9bbdc3b412810af9ac9a84d7066c
maxigomez99 <gomez.maximiliano99@gmail.com>
2025-06-21 16:27:24 -0300

    modificando tipo en imagen sucursal

--- src/main/java/com/buensabor/buensabor/entities/Sucursal.java
+++ src/main/java/com/buensabor/buensabor/entities/Sucursal.java
@@ -21,6 +21,9 @@ public class Sucursal extends Base {
     private Empresa empresa;
     @OneToOne(cascade = CascadeType.PERSIST)
     private Domicilio domicilio;
-    private String imagen;
+
+  @Lob
+  @Column(columnDefinition = "LONGTEXT")
+  private String imagen;it
 
 }
